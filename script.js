514d987169adceb3aad23ea31fedc3b28e44330c
Giovanni <giovanni.renda16@gmail.com>
2025-03-23 13:26:04 +0100

    added functionalities

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("js ok");
+
+const taskList = document.getElementById("taskList");
+const input = document.querySelector("input");
+
+function add() {
+    const task = input.value.trim();
+
+    if (task !== "") {
+    const newLi = document.createElement("li");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+    newLi.appendChild(checkBox);
+    newLi.appendChild(taskText);
+    taskList.appendChild(newLi);
+    input.value = "";
+    }
+}
